94f5eb71c61e93c032898abbf2d6c5173d7c967a
Mark Nenadov <marknenadov@gmail.com>
2011-04-18 12:29:43 -0400

    Added 1 mthd in YNAB3_Transaction. Added 5 mthds, removed 1 mthd, revised 1 mthd in YNAB3_Transaction_lister. Removed filter example from Parser.py

--- YNABpy/Parser.py
+++ YNABpy/Parser.py
@@ -49,7 +49,7 @@ class YNAB3_Transaction:
                           xmlize('outflow')]
 
     dom = None
- + " ($" + t.get_outflow() +
+
     def __init__(self, transaction_dom):
         """Constructor
 
@@ -311,6 +311,8 @@ class YNAB3_Parser:
 
 if __name__ == "__main__":
 
+    print("This is a module meant for being imported. Please refer to examples in the project folder!")
+
     YNAB_DATA_FILE = "F:/Development/PortableGit/YNABpy/YNABpy/test_budget.ynab3"
     yparser = YNAB3_Parser(YNAB_DATA_FILE)
 
@@ -323,43 +325,4 @@ if __name__ == "__main__":
     print("Percentage of transactions accepted across the database:" + \
           str(transaction_lister.get_pct_accepted()))
 
-    # test filtering transaction list based on payee substring
-
-    print("\nFilter by transactions with payees that match 'Canadian':\n")
-
-    for t in transaction_lister.get_transactions_by_payee('Canadian'):
-        print( "'" + t.get_payee()  + " ($" + t.get_outflow() + " outflow) ' matches 'Canadian'" )
-
-    # test filtering transaction list based on payee memo
-
-    print("\nFilter by transactions with payees that match 'immigration':\n")
-
-    for t in transaction_lister.get_transactions_by_memo('immigration'):
-        print( "'" + t.get_memo() + " ($" + t.get_outflow() + " outflow)' matches 'immigration'" )
-
-
-    # test inflow/outflow filtering
-
-    print( "\nTransactions going out that are between $1000 and $9000:" )
-    for t in transaction_lister.get_transactions_by_outflow_filter([1000, 9000]):
-        t_date = "?"
-
-        if t.get_payee() != None:
-            t_payee = t.get_payee()
-
-        if t.get_date() != None:
-            t_date = t.get_date()
-
-        print(t_payee + " (outflow of $"+str(t.get_outflow())+") on " + t_date + " )")
-
-    print( "\nTransactions coming in that are between $1000 and $9000:" )
-    for t in transaction_lister.get_transactions_by_inflow_filter([1000, 9000]):
-        t_date = "?"
-
-        if t.get_payee() != None:
-            t_payee = t.get_payee()
-
-        if t.get_date() != None:
-            t_date = t.get_date()
-
-        print(t_payee + " (inflow of $"+str(t.get_inflow())+") on " + t_date + " )")
+   
