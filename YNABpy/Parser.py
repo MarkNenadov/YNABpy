19ed13244461a5c43a9430d50c3f2c27d396884e
Mark Nenadov <marknenadov@gmail.com>
2011-04-19 08:56:17 -0400

    Moved the totals examples/tests out of Parser.py into totals.py in examples

--- YNABpy/Parser.py
+++ YNABpy/Parser.py
@@ -327,17 +327,4 @@ if __name__ == "__main__":
 
     print("This is a module meant for being imported. Please refer to examples in the project folder!")
 
-    YNAB_DATA_FILE = "F:/Development/PortableGit/YNABpy/YNABpy/test_budget.ynab3"
-    yparser = YNAB3_Parser(YNAB_DATA_FILE)
-
-
-    # test various total functions (total # payees, total percentage of accepted
-    # transactions, etc.)
-
-    transaction_lister = yparser.get_transaction_lister()
-    print("# of Payees across the database:" + str(len(transaction_lister.get_payees())))
-    print("# of Accounts across the database:" + str(len(transaction_lister.get_accounts())))
-    print("Percentage of transactions accepted across the database:" + \
-          str(transaction_lister.get_pct_accepted()))
-
    
---
+++ examples/totals.py
@@ -0,0 +1,15 @@
+# test various total functions (total # payees, total percentage of accepted
+# transactions, etc.)
+
+from YNABpy import Parser
+
+YNAB_DATA_FILE = "F:/Development/PortableGit/YNABpy/YNABpy/test_budget.ynab3"
+yparser = YNAB3_Parser(YNAB_DATA_FILE)
+
+transaction_lister = yparser.get_transaction_lister()
+print("# of Payees across the database:" + str(len(transaction_lister.get_payees())))
+print("# of Accounts across the database:" + str(len(transaction_lister.get_accounts())))
+print("Percentage of transactions accepted across the database:" + \
+      str(transaction_lister.get_pct_accepted()))
+
+
