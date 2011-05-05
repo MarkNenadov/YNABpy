6b5bbda8b288fecf0883ac1d80f81ca37574c973
Mark Nenadov <marknenadov@gmail.com>
2011-05-05 09:07:06 -0400

    Began work on Reporting, using reportlab. Intial setup.

--- YNABpy/Reporting.py
+++ YNABpy/Reporting.py
@@ -29,4 +29,53 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """
 
+import sys
 
+try:
+    from reportlab.pdfgen import canvas
+    from reportlab.rl_config import defaultPageSize
+    from reportlab.lib.units import cm
+except ImportError:
+    print("The reportlab module is a required for the Reporting parts of YNABpy. Please install that")
+    sys.exit(0)
+
+PAGE_CONFIG = {}
+PAGE_CONFIG['HEIGHT'] = defaultPageSize[1]
+PAGE_CONFIG['WIDTH'] = defaultPageSize[0]
+
+def CM(n):
+    """ return a value in centimeters
+    """
+
+    return n*cm
+
+class PDFReport:
+    """ Represents a YNABpy PDF report
+    """
+    
+    p = None
+
+    def __init__(self, file_name):
+        """ Constructor, initialize canvas
+        """
+
+        self.p = canvas.Canvas(file_name)
+ 
+    def draw(self):
+        """ Draw the PDF
+        """
+
+        self.p.rect(CM(0.8),CM(0.8), PAGE_CONFIG['WIDTH']-CM(1.6), \
+               PAGE_CONFIG['HEIGHT']-CM(1.6), fill=0)
+
+    def save(self):
+        """ Save out the PDF
+        """
+
+        self.p.showPage()
+        self.p.save()
+
+if __name__ == "__main__":
+    pr = PDFReport('test.pdf')
+    pr.draw()
+    pr.save()
